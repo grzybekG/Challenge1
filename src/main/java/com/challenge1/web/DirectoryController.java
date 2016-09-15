e4007d213dd58cd6e0788e719d85e2be7c497786
grzybekG <grzyb__ek@o2.pl>
2016-09-15 12:16:30 +0200

    Codecov.io analysis added. Import removal

--- src/main/java/com/challenge1/web/DirectoryController.java
+++ src/main/java/com/challenge1/web/DirectoryController.java
@@ -1,6 +1,5 @@
 package com.challenge1.web;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
--- src/test/java/com/challenge1/ReadStreamApplicationTest.java
+++ src/test/java/com/challenge1/ReadStreamApplicationTest.java
@@ -1,13 +1,9 @@
 package com.challenge1;
 
-import com.challenge1.config.WebSocketConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationContextLoader;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
