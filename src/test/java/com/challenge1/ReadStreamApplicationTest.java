8143cb49e610a7374dbcc8f4dc5a0abcecd41d8c
grzybekG <grzyb__ek@o2.pl>
2016-09-15 09:37:15 +0200

    Spring - test issue with reactive (known issue)

--- src/test/java/com/challenge1/ReadStreamApplicationTest.java
+++ src/test/java/com/challenge1/ReadStreamApplicationTest.java
@@ -1,16 +1,22 @@
 package com.challenge1;
 
+import com.challenge1.config.WebSocketConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationContextLoader;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ReadStreamApplicationTest {
 
 	@Test
 	public void contextLoads() {
+		//spring boot tests with reactive doesn't work for some reason - added integration test marker solve this issue.
+		//https://github.com/spring-projects/spring-boot/issues/4908
 	}
 
 }
