b8fc1751ebb7651db1d88b75f245b6fe88844ac9
San <grzyb__ek@o2.pl>
2016-09-19 19:05:21 +0200

    Path test (fix)

--- src/test/java/com/challenge1/service/FileServiceImplTest.java
+++ src/test/java/com/challenge1/service/FileServiceImplTest.java
@@ -2,14 +2,9 @@ package com.challenge1.service;
 
 import com.challenge1.service.api.FileService;
 import org.junit.Assert;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.springframework.util.ResourceUtils;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.net.URL;
 import java.util.Iterator;
 
 import static org.hamcrest.core.Is.is;
@@ -19,34 +14,9 @@ import static org.hamcrest.core.Is.is;
  */
 public class FileServiceImplTest {
 
-    public static String TEST_RESOURCE_PATH = "src/test/resources/ParentFolder";
-
-    @BeforeClass
-    public static void  setUp() throws FileNotFoundException {
-        System.out.print("PATH FOUND getLocation" + FileServiceImplTest.class.getProtectionDomain().getCodeSource().getLocation());
-    }
+    public static String TEST_RESOURCE_PATH = "./src/test/resources/ParentFolder";
 
     FileService fileService = new FileServiceImpl();
-
-    @Test
-    public void getCurrentFileStructure() {
-
-        Iterator<File> fileIterator = fileService.collectFoldersForPath(".");
-        StringBuilder sBuilder = new StringBuilder();
-        while (fileIterator.hasNext()) {
-            File next = fileIterator.next();
-            sBuilder.append("Abs Path: " + next.getAbsolutePath());
-            sBuilder.append(System.lineSeparator());
-            sBuilder.append("Path: " + next.getPath());
-            sBuilder.append(System.lineSeparator());
-
-        }
-
-        System.out.print("TESTPRINT " +sBuilder.toString());
-
-        directoryOnlyCheck(fileIterator);
-    }
-
     @Test
     public void shouldReturnOneFolderElementIterator() {
 
